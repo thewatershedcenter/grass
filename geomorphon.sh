e952240e963455359c2482337a63a3ee8c79159d
michael <michaelhuggins@protonmail.com>
2022-05-17 15:22:11 -0700

    edit geomorph.sh

--- geomorphon.sh
+++ geomorphon.sh
@@ -14,6 +14,12 @@ eval `cat /etc/os-release`
 # variable for grass exec for brevity
 EXEC="grass --tmp-location $EPSG --exec"
 
-$EXEC
+$EXEC `g.region raster=$DTM &&
+       r.slope.aspect elevation=$DTM slope=Slope &&
+       r.geomorphon -m elevation=Slope forms=Geomorph_25_15_7 search=25 skip=7 flat=15 dist=0 &&
+       r.mapcalc expression="<= 8, 0 ,1"`
 
+#g.region raster=$DTM
+#r.slope.aspect slope=Slope
+#r.geomorphon -m elevation=Slope
 #r.geomorphon -m elevation=$DTM
