e9abc315bf2e29169ed7ed162c5777b289cd9b9b
michael <michaelhuggins@protonmail.com>
2022-05-18 15:36:41 -0700

    added geomorphons_vice.sh

---
+++ geomorphon_vice.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+
+EPSG=$1
+DTM=$2
+AOI=$3
+
+# this
+eval `cat /etc/os-release`
+
+# grass exec for brevity
+EXEC="grass --tmp-location $EPSG --exec"
+
+# use cat to write script to get around constraints of --tmp-location
+# ************ sTaRt oF inTeRnAl sCrIpt *****************
+cat >/out/scriptception.sh <<EOF
+#!/bin/bash
+
+DTM=\$1
+AOI=\$2
+SRCH=\$3
+SKP=\$4
+FLT=\$5
+SZ=\$6
+
+# import dtm
+r.in.gdal input=/data/\$DTM output=dtm --overwrite
+
+# set region
+g.region raster=dtm
+echo "Region set from DTM:"
+g.region -p
+
+if [ -z \${AOI} ]
+then
+    # make output filename
+    f=\${DTM##*/}
+    OUT=/out/\${f%.*}_geomorph.tiff
+else
+    # set region to aoi
+    v.in.ogr input=/data/\$AOI output=aoi
+    g.region vector=aoi
+    echo "Region set from AOI:"
+    g.region -p
+    # make output filename
+    f=\${AOI##*/}
+    OUT=/out/\${f%.*}_geomorph.tiff
+fi
+
+# calc slope from dtm
+r.slope.aspect elevation=dtm slope=Slope
+
+# calc geomorphon
+r.geomorphon elevation=Slope forms=geomorph search=\$SRCH skip=\$SKP flat=\$FLT
+
+# map algeabra
+r.mapcalc expression="algeabra = if(geomorph <= 8, 0 ,1)"
+
+# neighborhood filter
+r.neighbors input=algeabra output=neighbor size=\$SZ method=sum
+
+# export tiff
+r.out.gdal input=neighbor output=\$OUT format=GTiff --overwrite
+EOF
+# ***************END oF inTeRnAl sCrIpt *****************
+
+# make script executable
+chmod +x /out/scriptception.sh # Make the script executable
+
+# now execute it
+$EXEC /out/scriptception.sh $DTM $AOI
+
+# remove the script
+rm /out/scriptception.sh
