bd202b3ffe093db09a34b32a88c9e507866f286a
michael <michaelhuggins@protonmail.com>
2022-05-17 17:41:05 -0700

    moved write of  scripception.sh ingeomorphon.sh

--- geomorphon.sh
+++ geomorphon.sh
@@ -16,7 +16,7 @@ EXEC="grass --tmp-location $EPSG --exec"
 
 # use cat to write script to get around constraints of --tmp-location
 # ************ sTaRt oF inTeRnAl sCrIpt *****************
-cat >scriptception.sh <<EOF
+cat >/out/scriptception.sh <<EOF
 #!/bin/bash
 EPSG=\$1
 DTM=\$2
@@ -31,15 +31,18 @@ r.slope.aspect elevation=\$DTM slope=Slope
 r.geomorphon -m elevation=Slope forms=Geomorph_25_15_7 search=25 skip=7 flat=15>
 
 # map algeabra
-r.mapcalc expression="<= 8, 0 ,1
+r.mapcalc expression="Geomorph_25_15_7  <= 8, 0 ,1
 EOF
 # ***************END oF inTeRnAl sCrIpt *****************
 
 
 # make script executable
-chmod +x scriptception.sh # Make the script executable
+chmod +x /out/scriptception.sh # Make the script executable
 
 # now execute it
-$EXEC scriptception.sh
+$EXEC /out/scriptception.sh
+
+# remove the script
+rm /out/scriptception.sh
 
 
